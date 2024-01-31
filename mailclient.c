ac497a2456660da11524ab546a5d9dd76cac7855
utsavdhanuka03 <utsavdhanuka03@kgpian.iitkgp.ac.in>
2024-01-31 18:29:42 +0530

    Second Commit

--- mailclient.c
+++ mailclient.c
@@ -9,134 +9,110 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 
-#define SMTP_SERVER "smtp.example.com"
+#define SMTP_SERVER "iitkgp.edu"
 #define SMTP_PORT 25
+#define MAX_BUFFER_SIZE 1024
 
-int main() {
-    int sock;
-    struct sockaddr_in server;
-    char message[1024], server_reply[2000];
-
-    // Create socket
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1) {
-        printf("Could not create socket");
-        return 1;
+int main(int argc, char *argv[])
+{
+    // the program takes 3 command line arguments
+    // server IP,smtp port,pop3port
+    if (argc != 4)
+    {
+        printf("Usage: %s <server IP> <smtp port> <pop3 port>\n", argv[0]);
+        exit(1);
     }
+    int sockfd;
+    struct sockaddr_in serv_addr;
 
-    server.sin_addr.s_addr = inet_addr(SMTP_SERVER);
-    server.sin_family = AF_INET;
-    server.sin_port = htons(SMTP_PORT);
-
-    // Connect to remote server
-    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
-        perror("Connect failed. Error");
-        return 1;
+    /* Opening a socket is exactly similar to the server process */
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("Unable to create socket\n");
+        exit(0);
     }
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    inet_aton(argv[1], &serv_addr.sin_addr);
+    serv_addr.sin_port = htons(atoi(argv[2]));
 
-    // Receive the server response
-    if (recv(sock, server_reply, 2000, 0) < 0) {
-        printf("Receive failed");
-        return 1;
+    // connect with server
+
+    char username[100];
+    char password[100];
+    printf("Enter username: ");
+    scanf("%s", username);
+    printf("Enter password: ");
+    scanf("%s", password);
+
+    while (1)
+    {
+        printf("Enter option:\n");
+        printf("1. Manage Mail\n");
+        printf("2. Send Mail\n");
+        printf("3. Quit\n");
+        int option;
+        scanf("%d", &option);
+        if (option == 1)
+        {
+            // manage mail
+            // send username and password to server
+            send(sockfd, username, strlen(username), 0);
+            send(sockfd, password, strlen(password), 0);
+            // receive number of mails
+            int num_mails;
+            recv(sockfd, &num_mails, sizeof(num_mails), 0);
+            printf("Number of mails: %d\n", num_mails);
+            // receive mails
+            for (int i = 0; i < num_mails; i++)
+            {
+                // receive mail
+                char mail[1000];
+                recv(sockfd, mail, sizeof(mail), 0);
+                printf("Mail %d:\n", i + 1);
+                printf("%s\n", mail);
+            }
+        }
+        else if (option == 2)
+        {
+            // connect to server
+            connect(sockfd, (struct sockaddr *)&serv_addr,
+                    sizeof(serv_addr));
+            // use SMTP protocol to send mail
+            char mail [MAX_BUFFER_SIZE];
+            char line [MAX_BUFFER_SIZE];
+            // get mail details from user 
+            fgets(mail,sizeof(mail), stdin);
+            // check format of mail
+            if(!isvalidmail(mail)){
+                printf("Invalid mail format\n");
+                continue;
+            }
+            else{
+                // write to server 
+                char *msg = "client connects to SMTP port>\n";
+                write(sockfd, msg, strlen(msg));
+                // read from server
+                read(sockfd, line, sizeof(line));
+                printf("%s\n", line);
+                // wrtie HELO domain
+                char *helo = "HELO iitkgp.edu\n";
+                write(sockfd, helo, strlen(helo));
+
+            }
+
+            
+        }
+        else if (option == 3)
+        {
+            // quit
+            break;
+        }
+        else
+        {
+            printf("Invalid option\n");
+        }
     }
-
-    printf("Server reply: %s\n", server_reply);
-
-    // Send EHLO command
-    strcpy(message, "EHLO example.com\r\n");
-    if (send(sock, message, strlen(message), 0) < 0) {
-        printf("Send failed");
-        return 1;
-    }
-
-    // Receive the server response
-    if (recv(sock, server_reply, 2000, 0) < 0) {
-        printf("Receive failed");
-        return 1;
-    }
-
-    printf("Server reply: %s\n", server_reply);
-
-    // Send MAIL FROM command
-    strcpy(message, "MAIL FROM: <sender@example.com>\r\n");
-    if (send(sock, message, strlen(message), 0) < 0) {
-        printf("Send failed");
-        return 1;
-    }
-
-    // Receive the server response
-    if (recv(sock, server_reply, 2000, 0) < 0) {
-        printf("Receive failed");
-        return 1;
-    }
-
-    printf("Server reply: %s\n", server_reply);
-
-    // Send RCPT TO command
-    strcpy(message, "RCPT TO: <recipient@example.com>\r\n");
-    if (send(sock, message, strlen(message), 0) < 0) {
-        printf("Send failed");
-        return 1;
-    }
-
-    // Receive the server response
-    if (recv(sock, server_reply, 2000, 0) < 0) {
-        printf("Receive failed");
-        return 1;
-    }
-
-    printf("Server reply: %s\n", server_reply);
-
-    // Send DATA command
-    strcpy(message, "DATA\r\n");
-    if (send(sock, message, strlen(message), 0) < 0) {
-        printf("Send failed");
-        return 1;
-    }
-
-    // Receive the server response
-    if (recv(sock, server_reply, 2000, 0) < 0) {
-        printf("Receive failed");
-        return 1;
-    }
-
-    printf("Server reply: %s\n", server_reply);
-
-    // Send email content
-    strcpy(message, "Subject: Test Email\r\n");
-    strcat(message, "From: sender@example.com\r\n");
-    strcat(message, "To: recipient@example.com\r\n");
-    strcat(message, "\r\n");
-    strcat(message, "This is the body of the email.\r\n");
-    strcat(message, ".\r\n");
-    if (send(sock, message, strlen(message), 0) < 0) {
-        printf("Send failed");
-        return 1;
-    }
-
-    // Receive the server response
-    if (recv(sock, server_reply, 2000, 0) < 0) {
-        printf("Receive failed");
-        return 1;
-    }
-
-    printf("Server reply: %s\n", server_reply);
-
-    // Send QUIT command
-    strcpy(message, "QUIT\r\n");
-    if (send(sock, message, strlen(message), 0) < 0) {
-        printf("Send failed");
-        return 1;
-    }
-
-    // Receive the server response
-    if (recv(sock, server_reply, 2000, 0) < 0) {
-        printf("Receive failed");
-        return 1;
-    }
-
-    printf("Server reply: %s\n", server_reply);
-
-    close(sock);
+    close(sockfd);
     return 0;
-}   
+}
--- smtpmail.c
+++ smtpmail.c
@@ -11,18 +11,71 @@
 
 #define MAX_BUFFER_SIZE 1024
 
-int main(int argc,int *argv[]) {
-    if (argc != 2) {
+void recieveMail(int clientSocket)
+{
+    char buffer[MAX_BUFFER_SIZE];
+    char username[255];
+    char sender[255];
+    // Read the email content from the client
+    // Read the username
+    // Mail is of format
+    /*From: <username>@<domain_name>
+ To: <username>@<domain_name>
+ Subject: <subject string, max 100 characters>
+ Received: <time at which received, in date : hour : minute>
+ <Message body – one or more lines> */
+    size_t n;
+    while ((n = read(clientSocket, buffer, MAX_BUFFER_SIZE)) > 0)
+    {
+        buffer[n] = '\0';
+        printf("%s", buffer);
+        if (strncmp(buffer, "From:", 5) == 0)
+        {
+            char *token = strtok(buffer, " ");
+            token = strtok(NULL, " ");
+            strcpy(sender, token);
+            printf("Sender is %s\n", sender);
+        }
+        else if (strncmp(buffer, "To:", 3) == 0)
+        {
+            char *token = strtok(buffer, " ");
+            token = strtok(NULL, " ");
+            strcpy(username, token);
+            printf("Username is %s\n", username);
+        }
+        else if (strncmp(buffer, "Subject:", 8) == 0)
+        {
+            char *token = strtok(buffer, " ");
+            token = strtok(NULL, " ");
+            printf("Subject is %s\n", token);
+        }
+        else if (strncmp(buffer, "Received:", 9) == 0)
+        {
+            char *token = strtok(buffer, " ");
+            token = strtok(NULL, " ");
+            printf("Received at %s\n", token);
+        }
+        else
+        {
+            printf("Message is %s\n", buffer);
+        }
+    }
+}
+int main(int argc, int *argv[])
+{
+    if (argc != 2)
+    {
         fprintf(stderr, "Usage: %s <my_port>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    int serverSocket, clientSocket;
+    int serverSocket;
     struct sockaddr_in serverAddress, clientAddress;
     char buffer[MAX_BUFFER_SIZE];
 
     // Create socket
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket == -1) {
+    if (serverSocket == -1)
+    {
         perror("Failed to create socket");
         exit(EXIT_FAILURE);
     }
@@ -35,46 +88,53 @@ int main(int argc,int *argv[]) {
     serverAddress.sin_port = htons(port);
 
     // Bind socket to address
-    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
+    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+    {
         perror("Failed to bind socket");
         exit(EXIT_FAILURE);
     }
 
     // Listen for incoming connections
-    if (listen(serverSocket, 5) < 0) {
+    if (listen(serverSocket, 5) < 0)
+    {
         perror("Failed to listen for connections");
         exit(EXIT_FAILURE);
     }
 
-    printf("SMTP server listening on port 25...\n");
+    printf("SMTP server listening on port %d...\n", port);
 
-    while (1) {
+    while (1)
+    {
         socklen_t clientAddressLength = sizeof(clientAddress);
 
         // Accept incoming connection
-        clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddress, &clientAddressLength);
-        if (clientSocket < 0) {
+        int clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddress, &clientAddressLength);
+        if (clientSocket < 0)
+        {
             perror("Failed to accept connection");
             exit(EXIT_FAILURE);
         }
 
         printf("New connection accepted\n");
+        pid_t pid = fork();
 
-        // Receive data from client
-        memset(buffer, 0, sizeof(buffer));
-        if (recv(clientSocket, buffer, sizeof(buffer), 0) < 0) {
-            perror("Failed to receive data from client");
+        if (pid < 0)
+        {
+            perror("Failed to fork");
             exit(EXIT_FAILURE);
         }
+        if (pid == 0)
+        {
+            close(serverSocket);
+            recieveMail(clientSocket, );
 
-        printf("Received data from client:\n%s\n", buffer);
-
-        // Process received data and send response
-
-        // TODO: Implement SMTP protocol logic here
-
-        // Close client socket
-        close(clientSocket);
+            // Process received data and send response
+        }
+        else
+        {
+            close(clientSocket);
+            continue;
+        }
     }
 
     // Close server socket
