f1c8d8e84c3daa5619109bf8a79352127df36d73
mr-195 <manaswiraj195@kgpian.iitkgp.ac.in>
2024-02-01 23:34:23 +0530

    cleaned files

--- gtest.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    // Unicode escape sequence for smiley face emoji
-    // U+1F604 represents the Unicode code point for the smiley face
-    printf("\U0001F604\n");
-
-    return 0;
-}
